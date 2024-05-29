cace5868766e194e23ec063867d83f0d97183164
ErickSito20 <erick182022@gmail.com>
2024-05-29 16:20:12 -0500

    practica 2

---
+++ 06-funciones-&-DOM/practica/app.js
@@ -0,0 +1,54 @@
+
+const miInput = document.getElementById("boton");
+
+function cambiarValue (){
+    console.log(miInput.value)
+    miInput.value = 'click exitoso! 😺'
+}
+
+miInput.addEventListener('click', cambiarValue)
+
+const cajaColor = document.getElementById('caja');
+const botonesColores = document.querySelectorAll ('.btnColores');
+
+console.log(cajaColor)
+console.log(botonesColores)
+
+function pintarCaja(e){ //e es como el escuchador, reune info del elemento cliqueado, target es el punto que le dio click, get atrribut trae un atributodel target y data color es el atributo que se esta trayendo 
+    console.log(e)
+    console.log(e.target.getAttribute('data-color'))
+    let newColor = e.target.getAttribute('data-color');
+    cajaColor.style.backgroundColor = newColor;
+}
+
+//eventos
+botonesColores[0].addEventListener('click',pintarCaja);
+botonesColores[1].addEventListener('click',pintarCaja);
+botonesColores[2].addEventListener('click',pintarCaja);
+botonesColores[3].addEventListener('click',pintarCaja);
+
+const pelota = document.getElementById('circulo');
+const btnMover = document.querySelector('#btnMover');
+let e = 0;
+
+
+function mover(){
+
+if (e == 0){
+    pelota.style.marginLeft = '400px';
+    pelota.style.backgroundColor = 'blue';
+    pelota.style.transitionDuration = '.6s';
+    e=1;
+}else{
+    pelota.style.marginLeft = '0px';
+    pelota.style.backgroundColor = 'black';
+    pelota.style.transitionDuration = '.6s';
+    e=0;
+}
+
+
+ 
+
+}
+
+btnMover.addEventListener('click',mover)
